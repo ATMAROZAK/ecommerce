13b66d64715941b9facda19512f2629e9e251295
Вадим <vadim@MacBook-Pro-Vadim.local>
2018-08-30 12:30:51 +0500

    first

---
+++ shop/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+#Модель бренда
+class Brand(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Бренд'
+        verbose_name_plural = 'Бренды'
+
+    def __str__(self):
+        return self.name
+
+# Модель категории
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+# Модель продукта
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products', verbose_name="Категория")
+    brand = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name="Бренд")
+    name = models.CharField(max_length=200, db_index=True, verbose_name="Название")
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/', blank=True, verbose_name="Изображение товара")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    stock = models.PositiveIntegerField(verbose_name="На складе")
+    available = models.BooleanField(default=True, verbose_name="Доступен")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        index_together = [
+            ['id', 'slug']
+        ]
+
+    def __str__(self):
+        return self.name
