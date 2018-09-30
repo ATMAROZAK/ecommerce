d020dba6435b7dccd83defbe5486f241d77270b6
Вадим <vadim@MBP-Vadim.Dlink>
2018-09-30 12:30:22 +0500

    Fix

--- cart/cart.py
+++ cart/cart.py
@@ -64,7 +64,7 @@ class Cart(object):
         for item in self.cart.values():
 
             item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
+            item['total_price'] = Decimal(item['price']) * Decimal(item['quantity'])
             yield item
 
     # Количество товаров
@@ -72,7 +72,7 @@ class Cart(object):
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+        return sum(Decimal(item['price']) * Decimal(item['quantity']) for item in self.cart.values())
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
