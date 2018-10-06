3411e2c0516b1816032a16a3e5f8ffd3a7434b21
Вадим <vadim@MacBook-Pro-Vadim.local>
2018-10-06 23:19:37 +0500

    add payeer

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/',include(('cart.urls', 'cart'), namespace='cart')),
     path('order/',include(('orders.urls', 'orders'), namespace='orders')),
-    path('payeer_656214683.txt/', payeer),
+    path('payeer_656214683.txt', payeer),
     path('', include(('shop.urls', 'shop'), namespace='shop')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
